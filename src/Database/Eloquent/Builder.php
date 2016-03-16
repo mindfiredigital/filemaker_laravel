7ea8a30601cbf8f381fa2a83877fffb7f89a6183
Lakin Mohapatra <lakin.mohapatra@Lakins-MacBook.local>
2016-03-16 11:32:11 +0530

    PSR-2 style fix for elobuilder

--- src/Database/Eloquent/Builder.php
+++ src/Database/Eloquent/Builder.php
@@ -1,28 +1,44 @@
 <?php namespace filemaker_laravel\Database\Eloquent;
 
+/**
++---------------------------------------------------------
+| File    : Builder.php
+| Path    : ./src/Database/Eloquent/Builder.php
+| Purpose : Contains laravel eloquent builder methods
+| Created : 9-Feb-2016
+| Author  : Lakin Mohapatra, Debabrata Patra
+| Company : Mindfire Solutions.
++---------------------------------------------------------
+*/
+
 use laravel_filemaker\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloBuilder;
 
+/**
+* Used to override methods of Eloquent builder
+*
+*  @see EloBuilder
+*/
 class Builder extends EloBuilder
 {
     /**
-     * Create a new Eloquent query builder instance.
-     *
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @return void
-     */
+    * Create a new Eloquent query builder instance.
+    *
+    * @param  laravel_filemaker\Database\Query\Builder  $query
+    * @return void
+    */
     public function __construct(QueryBuilder $query)
     {
         $this->query = $query;
     }
 
     /**
-     * Find a model by its primary key.
-     *
-     * @param  array  $ids
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
+    * Find a model by its primary key.
+    *
+    * @param  array  $ids
+    * @param  array  $columns
+    * @return \Illuminate\Database\Eloquent\Collection
+    */
     public function findMany($ids, $columns = ['*'])
     {
         if (empty($ids)) {
