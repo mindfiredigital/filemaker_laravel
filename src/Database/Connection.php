1d436bee6054f8a89b9e694ca1a4c96cba3af57a
Lakin Mohapatra <lakin.mohapatra@Lakins-MacBook.local>
2016-03-18 12:20:12 +0530

    Fix PHPCI code styles

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -1,8 +1,26 @@
 <?php namespace filemaker_laravel\Database;
 
+/**
+ +--------------------------------------------------------------------
+ |
+ | File    : Connection.php
+ | Path    : ./src/Database/Connection.php
+ | Purpose : Contains all methods of filemaker connections
+ | Created : 9-Feb-2016
+ | Author  : Lakin Mohapatra, Debabrata Patra
+ | Company : Mindfire Solutions.
+ | Comments:
+ +--------------------------------------------------------------------
+ */
+
 use Illuminate\Database\Connection as BaseConnection;
 use FileMaker;
 
+/**
+ * It manages the filemaker connection.
+ *
+ * @see \Illuminate\Database\Connection
+ */
 class Connection extends BaseConnection
 {
     /**
@@ -11,40 +29,55 @@ class Connection extends BaseConnection
      * @var Object - Filemaker DB
      */
     protected $db;
-    
+
     /**
      * The Filemaker connection handler.
      *
      * @var Object - Filemaker Databse connection handler
      */
     protected $connection;
-    
+
     /**
      * Create a new database connection instance.
      *
-     * @param  array   $config
+     * @param  Array   $config -hostname, username, password & database
+     * @return void
      */
     public function __construct(array $config)
     {
         $this->useDefaultQueryGrammar();
-		$this->useDefaultPostProcessor();
-		$this->config = $config;
+        $this->useDefaultPostProcessor();
+        $this->config = $config;
     }
-    
+
+    /**
+    * Gets filemaker connection Object
+    *
+    * @param void
+    * @return Object - FileMaker Connection Object
+    */
     public function getConnection()
-	{
+    {
         $config = $this->config;
         return $this->createFMConnection($config);
     }
-    
+
+    /**
+    * Creates filemaker connection
+    *
+    * @param Array $config - Array containing following indexes.
+    *                             - database
+    *                             - host
+    *                             - username
+    *                             - password
+    */
     public static function createFMConnection($config)
     {
         return new FileMaker(
-			$config['database'],
-			$config['host'],
-			$config['username'],
-			$config['password']
-		);
+            $config['database'],
+            $config['host'],
+            $config['username'],
+            $config['password']
+        );
     }
-    
 }
--- src/Database/FileMakerServiceProvider.php
+++ src/Database/FileMakerServiceProvider.php
@@ -1,20 +1,41 @@
 <?php namespace filemaker_laravel\Database;
 
+/**
+ +--------------------------------------------------------------------
+ |
+ | File    : FileMakerServiceProvider.php
+ | Path    : ./src/Database/FileMakerServiceProvider.php
+ | Purpose : Contains all methods of filemaker connections
+ | Created : 9-Feb-2016
+ | Author  : Lakin Mohapatra, Debabrata Patra
+ | Company : Mindfire Solutions.
+ | Comments:
+ +--------------------------------------------------------------------
+ */
+
 use filemaker_laravel\Database\Connection;
 use Illuminate\Support\ServiceProvider;
+
+/**
+* Creates serviceprovider for filemaker
+*
+* @see \Illuminate\Support\ServiceProvider
+*
+*/
 class FileMakerServiceProvider extends ServiceProvider
 {
     /**
      * Register the service provider.
      *
+     * @param void
      * @return void
      */
     public function register()
     {
-        $this->app->resolving('db', function($db)
-        {
-            $db->extend('filemaker', function($config)
-            {
+        $this->app->resolving('db', function ($db) {
+
+            $db->extend('filemaker', function ($config) {
+
                 return new Connection($config);
             });
         });
